4c17900187cc581f7e8d06d99bedf6403eff92ae
VighneshG28 <vighneshgole56@gmail.com>
2025-03-23 01:07:58 +0530

    updated createPost

--- src/Components/createPost.jsx
+++ src/Components/createPost.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [post, setPost] = useState("")
+
   return (
     <div>
-      createpost
+      <div>
+        <h1>Create Post</h1>
+        <div>
+          <label>Title : </label>
+          <input placeholder='Enter the title' onChange={(e) => {setTitle(e.target.value)}} />
+        </div>
+        <div>
+          <label>Post : </label>
+          <textarea placeholder='Enter the text' onChange={(e) => {setPost(e.target.value)}}></textarea>
+        </div>
+      </div>
     </div>
   )
 }
